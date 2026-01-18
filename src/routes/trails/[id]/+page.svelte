1a7d38883564da292411847fa3ef6ecaab035f31
tim_k <tim_kastner98@yahoo.de>
2026-01-18 12:57:36 +0100

    add ElevationProfile component to display trail elevation data

--- src/routes/trails/[id]/+page.svelte
+++ src/routes/trails/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import ElevationProfile from "$lib/components/ElevationProfile.svelte";
   import LeafletMap from "$lib/components/LeafletMap.svelte";
   import { trailService } from "$lib/services/trail-service";
   import { onMount } from "svelte";
@@ -28,34 +29,38 @@
     {/each}
   </div>
   <div class="row">
-    <div class="overview">
-      <div>
-        <h1>{data.trail?.name}</h1>
-        <p>{data.trail?.description}</p>
-        <p>Lat: {data.trail?.geometry.coordinates[0][1]}</p>
-        <p>Lng: {data.trail?.geometry.coordinates[0][0]}</p>
+    <div class="column">
+      <div class="overview">
+        <div>
+          <h1>{data.trail?.name}</h1>
+          <p>{data.trail?.description}</p>
+          <h4>Startingpoint:</h4>
+          <p>Lat: {data.trail?.geometry.coordinates[0][1]}</p>
+          <p>Lng: {data.trail?.geometry.coordinates[0][0]}</p>
+        </div>
+        <div class="actions">
+          <CldUploadWidget uploadPreset="unsigned_upload" let:open let:isLoading {onSuccess}>
+            <button onclick={() => open()} disabled={isLoading}> Add Image </button>
+          </CldUploadWidget>
+          <button onclick={() => goto(`/trails/${data.trail!._id}/edit`)}>Edit</button>
+          <button
+            type="button"
+            onclick={() =>
+              trailService
+                .deleteTrail(data.trail!._id, data.session!.token)
+                .then(() => {
+                  alert("Trail deleted successfully.");
+                  goto("/home");
+                })
+                .catch((error) => {
+                  alert("Error deleting trail: " + error.message);
+                })}
+          >
+            Delete
+          </button>
+        </div>
       </div>
-      <div class="actions">
-        <CldUploadWidget uploadPreset="unsigned_upload" let:open let:isLoading {onSuccess}>
-          <button onclick={() => open()} disabled={isLoading}> Add Image </button>
-        </CldUploadWidget>
-        <button onclick={() => goto(`/trails/${data.trail!._id}/edit`)}>Edit</button>
-        <button
-          type="button"
-          onclick={() =>
-            trailService
-              .deleteTrail(data.trail!._id, data.session!.token)
-              .then(() => {
-                alert("Trail deleted successfully.");
-                goto("/home");
-              })
-              .catch((error) => {
-                alert("Error deleting trail: " + error.message);
-              })}
-        >
-          Delete
-        </button>
-      </div>
+      <ElevationProfile trail={data.trail} />
     </div>
     <LeafletMap
       height={50}
@@ -92,6 +97,13 @@
     height: 50%;
     width: 100%;
     justify-content: center;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-right: 1rem;
   }
 
   .overview {
